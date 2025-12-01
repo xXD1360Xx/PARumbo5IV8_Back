b43fbb17663cddcfeec75588965a78714363e323
alexisgonzales2728-byte <alexisgonzales2728@gmail.com>
2025-11-30 18:56:37 -0600

    corregiste sintaxis de configuration

--- controladores/autenticacionControlador.js
+++ controladores/autenticacionControlador.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { pool } from '../configuration/basedeDatos.js';
+import { pool } from '../configuracion/basedeDatos.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
