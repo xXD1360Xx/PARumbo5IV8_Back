be553ea2ae4b18235357987358a702860535b241
alexisgonzales2728-byte <alexisgonzales2728@gmail.com>
2025-11-30 18:02:43 -0600

    first commit

---
+++ configuracion/basedeDatos.js
@@ -0,0 +1,15 @@
+// configuration/basedeDatos.js
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new pkg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 20000,
+  ssl: process.env.ENTORNO === 'produccion' ? { rejectUnauthorized: false } : false
+});
+
+export { pool };
